48747b14bae5330bfb41ad41202b59bbb30a3d2f
D. Stuart Freeman <stuart.freeman@et.gatech.edu>
2013-02-28 11:29:14 -0500

    add in kaltura auth portion

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var CAS = require('cas-sfu');
 var url = require('url');
+var crypto = require('crypto');
 var config = require('./config.json');
 
 var app = express();
 var cas = new CAS(config.cas);
+var shasum = crypto.createHash('sha1');
 
 app.get('/login', function(req, res){
     var ticket = req.param('ticket');
@@ -12,7 +14,7 @@ app.get('/login', function(req, res){
         // If we have a ticket in the parameters, we try to validate it.
         cas.validate(ticket, function(err, loggedIn, casResponse) {
             if (loggedIn) {
-                success(casResponse.user);
+                success(res, casResponse.user);
             } else {
                 // The CAS server thinks this is an invalid token.
                 return res.send(401, 'Invalid token');
@@ -20,7 +22,8 @@ app.get('/login', function(req, res){
         });
     } else {
         // No ticket, redirect to cas login
-        var redirectUrl = url.parse(config.cas.casHost + config.cas.loginPath, true);
+        var redirectUrl = url.parse(config.cas.casHost +
+            config.cas.casBasePath + config.cas.loginPath, true);
         redirectUrl.query.service = config.cas.service;
         if (config.cas.allow) {
             redirectUrl.query.allow = config.cas.allow;
@@ -29,8 +32,19 @@ app.get('/login', function(req, res){
     }
 });
 
-var success = function(user) {
-    console.log(user);
+var success = function(res, userId) {
+    var userRole = config.kaltura.roles[userId] || config.kaltura.defaultRole;
+    var salt = config.kaltura.secret;
+    // Expires in 24 hours
+    var expires = Date.now() / 1000 + (60 * 60 * 24);
+    // random number from 0-32000
+    var r = Math.floor(Math.random() * 32000);
+    var info = userId + ';' + userRole + ';;' + expires + ';' + r;
+    shasum.update(salt + info);
+    var signature = shasum.digest('hex');
+    var hashed = new Buffer(signature + '|' + info).toString('base64');
+    res.redirect(config.kaltura.host +
+            '/mediaspacefolder/user/authenticate/sessionKey/' + hashed);
 };
 
 app.listen(8080);
